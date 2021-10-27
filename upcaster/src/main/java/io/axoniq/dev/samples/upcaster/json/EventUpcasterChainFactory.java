0e9ec22f036a597a58f1fbcb7e43b402ac9feaec
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-10-27 12:29:54 +0200

    Add configuration method
    Add a method that use the Configurer to configure all the upcasters.
    This sample would be usable when a users isn't based on Spring (Boot)
    
    #one-to-many-upcaster

--- upcaster/src/main/java/io/axoniq/dev/samples/upcaster/json/EventUpcasterChainFactory.java
+++ upcaster/src/main/java/io/axoniq/dev/samples/upcaster/json/EventUpcasterChainFactory.java
@@ -1,6 +1,9 @@
 package io.axoniq.dev.samples.upcaster.json;
 
+import org.axonframework.config.Configurer;
 import org.axonframework.serialization.upcasting.event.EventUpcasterChain;
+
+import java.util.function.Function;
 
 /**
  * Utility class constructing the {@link EventUpcasterChain} to configure on the {@link
@@ -8,6 +11,10 @@ import org.axonframework.serialization.upcasting.event.EventUpcasterChain;
  * <p>
  * In a Spring Boot environment exposing an {@code EventUpcasterChain} bean is sufficient for the framework to pick it
  * up correctly. To that end we can use the {@link #buildEventUpcasterChain()} method.
+ * <p>
+ * When using Axon's {@link org.axonframework.config.Configurer} directly, you should configure all upcasters separately
+ * by invoking the {@link org.axonframework.config.Configurer#registerEventUpcaster(Function)} method. The {@link
+ * #configureUpcasters(Configurer)} shows how we should implement this.
  *
  * @author Yvonne Ceelie
  */
@@ -24,6 +31,17 @@ public abstract class EventUpcasterChainFactory {
         return new EventUpcasterChain(
                 new FlightDelayedEvent0_to_1Upcaster()
         );
+    }
+
+    /**
+     * Configures all the upcasters of this application with the given {@code configurer}.
+     * <p>
+     * Can be utilized to configure upcasters if the application code uses Axon's {@link Configurer} directly.
+     *
+     * @param configurer the {@link Configurer} to register all upcasters with
+     */
+    public static void configureUpcasters(Configurer configurer) {
+        configurer.registerEventUpcaster(config -> new FlightDelayedEvent0_to_1Upcaster());
     }
 
     private EventUpcasterChainFactory() {
