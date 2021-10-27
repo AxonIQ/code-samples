ea0dd1f9d5a922b3e401fa426602f84bc887ab18
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-10-27 15:52:46 +0200

    Add base for sequencing-policy
    Add base for sequencing-policy. Also introduce the axon-bom to the
    code-samples project.
    
    #custom-sequencing-policy

---
+++ sequencing-policy/src/test/java/io/axoniq/dev/samples/sequencingpolicy/SequencingPolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.axoniq.dev.samples.sequencingpolicy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SequencingPolicyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
