4abe61b07e2b7f46fa5fb0bf240b481000f2fca8
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-10-28 12:39:34 +0200

    Add configuration
    Add configuration to either toggle between the custom SequencingPolicy
    or the PropertySequencingPolicy
    
    #custom-sequencing-policy

--- sequencing-policy/src/main/java/io/axoniq/dev/samples/sequencingpolicy/SequencingPolicyApplication.java
+++ sequencing-policy/src/main/java/io/axoniq/dev/samples/sequencingpolicy/SequencingPolicyApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SequencingPolicyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SequencingPolicyApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SequencingPolicyApplication.class, args);
+    }
 }
