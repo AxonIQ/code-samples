38d410332fdca706fe26fcc4125dfc39915e7ba9
Your Name <you@example.com>
2024-09-10 12:28:11 +0200

    workshop setup

--- multitenancy/src/main/java/io/axoniq/multitenancy/MultiTenantConfig.java
+++ multitenancy/src/main/java/io/axoniq/multitenancy/MultiTenantConfig.java
@@ -1,12 +1,6 @@
 package io.axoniq.multitenancy;
 
-import org.axonframework.axonserver.connector.AxonServerConfiguration;
-import org.axonframework.axonserver.connector.AxonServerConnectionManager;
-import org.axonframework.axonserver.connector.event.axon.AxonServerEventStore;
-import org.axonframework.eventsourcing.snapshotting.SnapshotFilter;
 import org.axonframework.extensions.multitenancy.components.TenantConnectPredicate;
-
-import org.axonframework.extensions.multitenancy.configuration.MultiTenantStreamableMessageSourceProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,16 +22,5 @@ public class MultiTenantConfig {
                                                           .factory());
     }
 
-    // UNCOMMENT THIS BEAN TO ENABLE MULTITENANCY WITH MULTIPLE DATA SOURCES
-//    @Bean
-//    public Function<TenantDescriptor, DataSourceProperties> tenantDataSourceResolver() {
-//        return tenant -> {
-//            DataSourceProperties properties = new DataSourceProperties();
-//            properties.setUrl("jdbc:h2:mem:" + tenant.tenantId());
-//            properties.setDriverClassName("org.h2.Driver");
-//            properties.setUsername("sa");
-//            return properties;
-//        };
-//    }
 }
 
