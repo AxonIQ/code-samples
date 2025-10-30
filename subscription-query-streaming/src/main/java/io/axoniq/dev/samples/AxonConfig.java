6bed3d00446a4ffae6567697a71b51bdb3fafd47
Mateusz Nowak <mateusz@nakodach.pl>
2025-10-30 12:45:06 +0100

    fix(axon-spring-boot-autoconfigure): do not duplicate interceptor registration

--- subscription-query-streaming/src/main/java/io/axoniq/dev/samples/AxonConfig.java
+++ subscription-query-streaming/src/main/java/io/axoniq/dev/samples/AxonConfig.java
@@ -1,8 +1,7 @@
 package io.axoniq.dev.samples;
 
-import org.axonframework.config.ConfigurerModule;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.eventhandling.gateway.EventGateway;
-import org.axonframework.lifecycle.Phase;
 import org.axonframework.messaging.Message;
 import org.axonframework.messaging.interceptors.LoggingInterceptor;
 import org.axonframework.queryhandling.QueryGateway;
@@ -10,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Basic Axon configuration adding the {@link LoggingInterceptor} to the {@link QueryGateway} and {@link EventGateway}.
+ * Basic Axon configuration adding the {@link LoggingInterceptor} to the {@link CommandGateway}, {@link QueryGateway} and {@link EventGateway}.
  * This provides easy logging of all the messages being published, ensuring that portion of the application works.
  *
  * @author Steven van Beelen
@@ -23,12 +22,4 @@ public class AxonConfig {
         return new LoggingInterceptor<>();
     }
 
-    @SuppressWarnings("resource")
-    @Bean
-    public ConfigurerModule loggingInterceptorConfigurerModule(LoggingInterceptor<Message<?>> loggingInterceptor) {
-        return configurer -> configurer.onInitialize(config -> config.onStart(Phase.OUTBOUND_QUERY_CONNECTORS, () -> {
-            config.eventGateway().registerDispatchInterceptor(loggingInterceptor);
-            config.queryGateway().registerDispatchInterceptor(loggingInterceptor);
-        }));
-    }
 }
